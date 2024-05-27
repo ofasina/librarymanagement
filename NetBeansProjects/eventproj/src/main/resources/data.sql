550f4d5ef870926046043baa6fade304a003dc5d
oluwatosin247 <oluwatosin.fasian@wayapaychat.com>
2024-05-27 09:17:12 +0100

    llibrary

--- NetBeansProjects/eventproj/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
--- Insert EVENTS
-INSERT INTO events(id, name, eventDescription, category, maxAttendees, date)
-VALUES (1, 'FA CUP FINAL', 'ARSNEAL VS MAN-U', 'GAME', '2024-02-15') ON CONFLICT DO NOTHING;
